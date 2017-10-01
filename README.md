89044d5d53c324393de24a0788900461c9d4e8fb
Daniel Mancuso <daniel.mancuso@bt.com>
2017-10-01 12:41:32 +0100

    Database seed added

--- README.md
+++ README.md
@@ -3,3 +3,28 @@ A Lavarel-Angular-Restful Application
 
 to run the seeder run the command
 php artisan db:seed --class=ImageUploader
+
+#SQL
+CREATE SCHEMA `twelfthman` DEFAULT CHARACTER SET latin1 ;
+grant all on twelfthman.* to 'root' identified by 'root';
+CREATE TABLE `twelfthman`.`images` (
+  `images_id` INT NOT NULL AUTO_INCREMENT,
+  `image_data` MEDIUMBLOB NULL,
+  `image_name` VARCHAR(100) NULL,
+  `image_alt_text` VARCHAR(45) NULL,
+  `image_ext` TINYTEXT NULL,
+  `deleted` INT NULL DEFAULT 0,
+  PRIMARY KEY (`images_id`),
+  UNIQUE INDEX `images_id_UNIQUE` (`images_id` ASC));
+)
+
+TODO: Create migrations script app/database/migrations/
+public function up()
+{
+    Schema::create('images', function(Blueprint $table)
+    {
+        $table->increments('images_id');
+        ...
+    });
+}
+
--- database/seeds/ImageUploader.php
+++ database/seeds/ImageUploader.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\Filesystem;
 
 class ImageUploader extends Seeder
 {
@@ -8,9 +10,38 @@ class ImageUploader extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * 
      */
     public function run()
     {
-        //
+
+        $filesystem = new Filesystem();
+
+        $from = base_path('public/import/images');
+
+        $images = $filesystem->allFiles($from);
+
+        foreach ($images as $key => $image) {
+
+        	if($filesystem->size($image->getPathname()) > 2000000 ){
+        		//TODO: resize the file if is too big
+        		echo ' WARNING! '.$image->getPathname().' is too big: Please optimize this image to less than 2MB and try again ';
+
+        	}else{
+        		//copy the original file to storage
+        		$path = 'public/images/'.$image->getFilename();
+        		$imageFile = $filesystem->get($image->getPathname());
+        		Storage::put($path, $imageFile);
+
+        		//save a copy in the database and give a unique name
+	        	DB::table('images')->insert([
+		            'image_data' => file_get_contents($image),
+		            'image_name' => date( 'Y-m-d' ) . '-' . str_random( 10 ) . $image->getFilename(),
+		            'image_alt_text' => $image->getFilename(),
+		            'image_ext' => $filesystem->extension($image->getPathname()),
+		            'deleted' => 0
+		        ]);
+        	}
+        }
     }
 }
